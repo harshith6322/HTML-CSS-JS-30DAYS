d34c5b359b7f06283d8a1785974456e2fbaa07c8
harshithreddy6322 <161709039+harshith6322@users.noreply.github.com>
2024-04-24 21:44:42 +0530

    Add files via upload

---
+++ day-21-preloader/script.js
@@ -0,0 +1,17 @@
+const pre = document.querySelector("#preloader");
+window.addEventListener("load", () => {
+  pre.style.display = "none";
+});
+
+const bar = document.querySelector(".bi-list");
+const close = document.querySelector(".bi-x-lg");
+const body = document.querySelector("body");
+const left = document.querySelector(".nav-left");
+bar.addEventListener("click", () => {
+  left.classList.add("addleft");
+  body.classList.add("body");
+});
+close.addEventListener("click", () => {
+  left.classList.remove("addleft");
+  body.classList.remove("body");
+});
